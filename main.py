d3fdd65e0f38a5965fee47d7f0a6980680db83c6
Viktor Kopp <vifactor@gmail.com>
2014-06-24 15:08:30 +0200

    Experimental and Fit maps are plotted

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import csv, sys
+import xrayutilities as xu
+import matplotlib.pyplot as plt
 
 def loadCSV(path):
     """loads a special csv-like file, result of RSM fit"""
@@ -28,4 +30,31 @@ def loadCSV(path):
     return x, y, zexp, zini, zfin
     
 if __name__ == '__main__':
-    print 'hello'
+    path = '/home/vifactor/Research/Samples/9543/XRD/FIT_misfit_threading_v5.0/folder/3.5deg-2.0deg-rsm-5h_6_9543_224_small.ft'
+    x, y, zexp, zini, zfin = loadCSV(path)
+    
+    gridder = xu.Gridder2D(25, 25)
+    
+    plt.figure()
+    
+    #Experimental map
+    ax = plt.subplot(221)
+    gridder(x, y, zexp)
+    LOGINT = xu.maplog(gridder.data.transpose(),6,0)
+    #draw rsm
+    cs = ax.contourf(gridder.xaxis, gridder.yaxis, LOGINT, 25, extend='min')
+    #annotate axis
+    ax.set_xlabel(r'$Q_{x}$')
+    ax.set_ylabel(r'$Q_{z}$')
+    
+    #Fit map
+    ax = plt.subplot(223)
+    gridder(x, y, zfin)
+    LOGINT = xu.maplog(gridder.data.transpose(),6,0)
+    #draw rsm
+    cs = ax.contourf(gridder.xaxis, gridder.yaxis, LOGINT, 25, extend='min')
+    #annotate axis
+    ax.set_xlabel(r'$Q_{x}$')
+    ax.set_ylabel(r'$Q_{z}$')
+    
+    plt.show()
